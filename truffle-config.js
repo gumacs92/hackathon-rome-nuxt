519fc2f69d31cb5554f39edc856bce0e11029829
Krisztian Barta <bartariki@gmail.com>
2022-06-16 23:39:18 +0200

    Mumbai Deployment

--- truffle-config.js
+++ truffle-config.js
@@ -10,13 +10,15 @@ module.exports = {
       host: "127.0.0.1"
     },
     matic_mumbai: {
-      provider: () => new HDWalletProvider(mnemonic, `https://matic-mumbai.chainstacklabs.com/`),
+      provider: () => new HDWalletProvider(mnemonic, `https://polygon-mumbai.g.alchemy.com/v2/hcFrhMzqBw5qNFrG25xGAXRU4sMciI2W`),
       network_id: 80001,
-      from: '0x4498Bdd0f655Bd1f21cd89840Ac73feb6b547bb6',
-      confirmations: 2,
+      //from: '0xEed63211B522d264d2EaFa59540114eb073491f6',
+      //confirmations: 2,
+      gas:4500000,
+      gasPrice:10000000000,
       timeoutBlocks: 200,
       // disableConfirmationListener: true,
-      skipDryRun: true
+      //skipDryRun: true
     },
   },
   mocha: {},
